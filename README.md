c4b301e696fdfe8a57c148ef83b3b0bcc207b86f
MoisesLoar18exit <moises.loartec@gmail.com>
2021-03-25 21:00:16 -0500

    flutter fundamentals

---
+++ README.md
@@ -0,0 +1,5 @@
+# Flutter Fundamentals Excercise
+Flutter Fundamentals Exercise with FloatingActionButton and Scaffold
+
+## Preview
+![Previsualización de la app](/img1.PNG)
---
+++ lib/src/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/src/pages/Contador.page.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Center(
+        child: ContadorPage(),
+      ),
+    );
+  }
+}
